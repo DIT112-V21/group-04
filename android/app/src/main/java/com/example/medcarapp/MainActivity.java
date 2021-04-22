d4131ceb1ac230224da900759761e1310d080b96
NilsDunlop <gusdunlni@student.gu.se>
2021-04-22 16:42:39 +0200

    #21 Add a highlight when selected a car option in recycleview
    Co-authored-by: JihanP <jihan8497@gmail.com>
    Co-authored-by: MarkusJ299 <gusjarmap@student.gu.se>
    Co-authored-by: Marble879 <younisakel879@gmail.com>

--- android/app/src/main/java/com/example/medcarapp/MainActivity.java
+++ android/app/src/main/java/com/example/medcarapp/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
