28908b87bdbec5558fadbc70f6f148a7cba44eda
Jihan Phanivong <jihan8497@gmail.com>
2021-04-27 15:27:18 +0200

    #14 Arduino sketch working with MQTT
    Co-authored-by: NilsDunlop <gusdunlni@student.gu.se>
    Co-authored-by: puerto-hub <guspuema@student.gu.se>
    Co-authored-by: gusasino <gusasino@student.gu.se>
    Co-authored-by: kevinayad <Kevinayad@hotmail.com>
    Co-authored-by: MarkusJ299 <gusjarmap@student.gu.se>
    Co-authored-by: Marble879 <younisakel879@gmail.com>

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -1,9 +1,79 @@
+#include <vector>
+
+#include <MQTT.h>
+#include <WiFi.h>
+#ifdef __SMCE__
+#include <OV767X.h>
+#endif
+
 #include <Smartcar.h>
 
+#ifndef __SMCE__
+WiFiClient net;
+#endif
+MQTTClient mqtt;
+
+ArduinoRuntime arduinoRuntime;
+BrushedMotor leftMotor(arduinoRuntime, smartcarlib::pins::v2::leftMotorPins);
+BrushedMotor rightMotor(arduinoRuntime, smartcarlib::pins::v2::rightMotorPins);
+DifferentialControl control(leftMotor, rightMotor);
+
+SimpleCar car(control);
+
+const auto oneSecond = 1000UL;
+const auto triggerPin = 6;
+const auto echoPin = 7;
+const auto maxDistance = 400;
+SR04 front(arduinoRuntime, triggerPin, echoPin, maxDistance);
+
+std::vector<char> frameBuffer;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+#ifdef __SMCE__
+  Camera.begin(QVGA, RGB888, 15);
+  frameBuffer.resize(Camera.width() * Camera.height() * Camera.bytesPerPixel());
+  mqtt.begin("3.138.188.190", 1883, WiFi);
+  // mqtt.begin(WiFi); // Will connect to localhost
+#else
+  mqtt.begin(net);
+#endif
+  if (mqtt.connect("arduino", "public", "public")) {
+    mqtt.subscribe("/smartcar/control/#", 1);
+    mqtt.onMessage([](String topic, String message) {
+      if (topic == "/smartcar/control/throttle") {
+        car.setSpeed(message.toInt());
+      } else if (topic == "/smartcar/control/steering") {
+        car.setAngle(message.toInt());
+      } else {
+        Serial.println(topic + " " + message);
+      }
+    });
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (mqtt.connected()) {
+    mqtt.loop();
+    const auto currentTime = millis();
+#ifdef __SMCE__
+    static auto previousFrame = 0UL;
+    if (currentTime - previousFrame >= 65) {
+      previousFrame = currentTime;
+      Camera.readFrame(frameBuffer.data());
+      mqtt.publish("/smartcar/camera", frameBuffer.data(), frameBuffer.size(),
+                   false, 0);
+    }
+#endif
+    static auto previousTransmission = 0UL;
+    if (currentTime - previousTransmission >= oneSecond) {
+      previousTransmission = currentTime;
+      const auto distance = String(front.getDistance());
+      mqtt.publish("/smartcar/ultrasound/front", distance);
+    }
+  }
+#ifdef __SMCE__
+  // Avoid over-using the CPU if we are running in the emulator
+  delay(35);
+#endif
 }
