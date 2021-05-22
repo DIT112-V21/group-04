ecc6f1498f7262c2cd6f5f5ee76cd6530e314d29
NilsDunlop <gusdunlni@student.gu.se>
2021-05-22 22:10:11 +0200

    #55 Add introductory activity test and test dependencies

---
+++ android/app/src/androidTest/java/com/example/medcarapp/IntroductoryActivityTest.java
@@ -0,0 +1,47 @@
+package com.example.medcarapp;
+
+import android.content.Context;
+import android.view.View;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.rule.ActivityTestRule;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class IntroductoryActivityTest {
+
+    private IntroductoryActivity introductoryActivity;
+    private ServerSelection serverSelection;
+
+    @Rule
+    public ActivityTestRule<IntroductoryActivity> introductoryActivityActivityTestRule = new ActivityTestRule<>(IntroductoryActivity.class);
+
+    @Rule ActivityTestRule<ServerSelection> serverSelectionActivityTestRule = new ActivityTestRule<>(ServerSelection.class,true,false);
+
+    @Before
+    public void setUp() throws Exception {
+        introductoryActivity = introductoryActivityActivityTestRule.getActivity();
+        Intents.init();
+    }
+
+    @Test
+    public void useAppContext() {
+        Context appContext = getInstrumentation().getTargetContext();
+        assertEquals("com.example.medcarapp", appContext.getPackageName());
+    }
+
+    @Test
+    public void verifyLoadedIds(){
+        View view = introductoryActivity.findViewById(R.id.logo);
+        assertNotNull(view);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        introductoryActivity = null;
+        Intents.release();
+    }
+}
