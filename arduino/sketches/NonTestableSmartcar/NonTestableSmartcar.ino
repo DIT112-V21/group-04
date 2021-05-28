b9205ec490cc89e1ea10ccb42d80712359ffae5b
Marble879 <younisakel879@gmail.com>
2021-05-28 03:25:07 +0300

    #68 Use the testable arduino sketch

---
+++ arduino/sketches/NonTestableSmartcar/NonTestableSmartcar.ino
@@ -0,0 +1,182 @@
+#include <vector>
+
+#include <MQTT.h>
+#include <WiFi.h>
+#ifdef __SMCE__
+#include <OV767X.h>
+#endif
+
+#include <Smartcar.h>
+
+
+#ifndef __SMCE__
+WiFiClient net;
+#endif
+MQTTClient mqtt;
+
+ArduinoRuntime arduinoRuntime;
+BrushedMotor leftMotor(arduinoRuntime, smartcarlib::pins::v2::leftMotorPins);
+BrushedMotor rightMotor(arduinoRuntime, smartcarlib::pins::v2::rightMotorPins);
+DifferentialControl control(leftMotor, rightMotor);
+
+SimpleCar car(control);
+
+
+const auto oneSecond = 1UL;
+const auto triggerPin = 6;
+const auto echoPin = 7;
+const auto maxDistance = 400;
+const int TRIGGER_PIN           = 6; // D6
+const int ECHO_PIN              = 7; // D7
+const unsigned int MAX_DISTANCE = 100;
+const auto BACK_PIN = 3;
+const auto stoppingSpeed = 0; 
+const auto stopDistanceFront = 80;
+const auto stopDistanceBack = 100;
+const int stopAngle = 0;
+const int autoSpeed = 65;
+const int autoAngle = 90;
+const int turnRight = 1;
+const int turnLeft = -1;
+int autoDriving = 0;
+int carSpeed = 0;
+boolean isObstacleDetectedPublished = false; //keeps track of when an obstacle has been detected message is published to mqtt
+
+
+SR04 frontSensorUS(arduinoRuntime, triggerPin, echoPin, maxDistance);
+GP2D120 backSensorIR(arduinoRuntime, BACK_PIN);
+
+std::vector<char> frameBuffer;
+
+void setup() {
+  Serial.begin(9600);
+#ifdef __SMCE__
+  //mqtt.begin("3.138.188.190", 1883, WiFi);
+  //mqtt.begin("aerostun.dev", 1883, WiFi);
+  mqtt.begin(WiFi); // Will connect to localhost
+  Camera.begin(QQVGA, RGB888, 15);
+  frameBuffer.resize(Camera.width() * Camera.height() * Camera.bytesPerPixel());
+#else
+  mqtt.begin(net);
+#endif
+  if (mqtt.connect("arduino", "public", "public")) {
+    Serial.println("GENERAL CONNECTION");
+    mqtt.subscribe("/smartcar/switchServer", 0);
+    mqtt.subscribe("/smartcar/control/#", 0);
+    mqtt.onMessage(+[](String& topic, String& message) {
+      Serial.println("Got initial message");
+      if (topic == "/smartcar/switchServer"){
+        Serial.println("SWITCHED");
+        mqtt.setHost("3.138.188.190", 1883);
+        mqtt.connect("arduino", "public", "public");
+        mqtt.subscribe("/smartcar/control/#", 0);
+        mqtt.publish("test", "test");
+      }
+      if (topic == "/smartcar/control/speed" && autoDriving == 0) {
+        carSpeed = message.toInt();
+        if (!(obstacleAvoidance())){
+          car.setSpeed(carSpeed);
+        }
+      } else if (topic == "/smartcar/control/turning" && autoDriving == 0) {
+        car.setAngle(message.toInt());
+      } else if (topic == "/smartcar/control/auto") {
+        autoDriving = message.toInt();
+        Serial.println(autoDriving);
+        if (autoDriving == 0){
+          car.setSpeed(stoppingSpeed);
+          car.setAngle(stopAngle);
+          carSpeed = stoppingSpeed;
+        } else {
+          car.setSpeed(autoSpeed);
+          carSpeed = autoSpeed;
+        }
+      } else {
+        Serial.println(topic + " " + message);
+      }
+    });
+  }
+}
+
+void loop() {
+  if (mqtt.connected()) {
+    mqtt.loop();
+    const auto currentTime = millis();
+#ifdef __SMCE__
+    static auto previousTransmission = 0UL;
+    if (currentTime - previousTransmission >= oneSecond) {
+      previousTransmission = currentTime;
+      Camera.readFrame(frameBuffer.data());
+      mqtt.publish("Camera_Stream", frameBuffer.data(), frameBuffer.size(), false, 0);
+    }
+#endif
+  }
+#ifdef __SMCE__
+  // Avoid over-using the CPU if we are running in the emulator
+  if (obstacleAvoidance()) {
+         if (autoDriving == 1){
+              autonomousMoving();
+          }
+      }
+  delay(35);
+#endif
+}
+
+boolean obstacleAvoidance(){
+  boolean isObstacleDetected = false;
+  
+  auto frontDistanceFromObject = frontSensorUS.getDistance();
+  auto backDistanceFromObject = backSensorIR.getDistance();
+  
+  boolean isFrontDetected = frontDistanceFromObject < stopDistanceFront && frontDistanceFromObject > 1 && (carSpeed > 0);
+  boolean isBackDetected = backDistanceFromObject < stopDistanceBack && backDistanceFromObject > 1 && (carSpeed < 0);
+  
+  
+  if (isFrontDetected || isBackDetected) {
+    sendObstacleDetectedNotification(true);
+    car.setSpeed(stoppingSpeed);
+    isObstacleDetected = true;
+  } else {
+    sendObstacleDetectedNotification(false);
+  }
+  
+  return isObstacleDetected;
+}
+
+
+void sendObstacleDetectedNotification(boolean shouldSend){
+  if (shouldSend){
+    if (!isObstacleDetectedPublished){
+      mqtt.publish("/smartcar/obstacle");
+      isObstacleDetectedPublished = true;
+    }
+  } else {
+      isObstacleDetectedPublished = false;
+  }
+}  
+
+void autonomousMoving(){
+        car.setSpeed(stoppingSpeed);
+        delay(500);
+        car.setSpeed(-autoSpeed);
+        delay(500);
+        car.setSpeed(stoppingSpeed);
+        delay(500);
+        turning(turnLeft);
+if (obstacleAvoidance()) {
+    turning(turnRight);
+    turning(turnRight);
+    if (obstacleAvoidance()) {
+            turning(turnRight);
+          }
+        }
+        car.setSpeed(autoSpeed);
+}
+
+void turning(int direction){
+  car.setSpeed(autoSpeed);
+  car.setAngle(direction*autoAngle);
+  delay(2000);
+  car.setSpeed(stoppingSpeed);
+  car.setAngle(stopAngle);
+  delay(500);
+}
--- arduino/sketches/smartcar/smartcar.ino
+++ arduino/sketches/smartcar/smartcar.ino
@@ -8,10 +8,14 @@
 
 #include <Smartcar.h>
 
+#include "SimpleCarMqttImplementation.h"
+#include "SimpleCarWrapper.h"
+#include "SimpleCarController.h"
+#include "SerialImplementation.h"
+#include "FrontDistanceSensorImplementation.h"
+#include "BackDistanceSensorImplementation.h"
 
-#ifndef __SMCE__
-WiFiClient net;
-#endif
+
 MQTTClient mqtt;
 
 ArduinoRuntime arduinoRuntime;
@@ -21,8 +25,18 @@ DifferentialControl control(leftMotor, rightMotor);
 
 SimpleCar car(control);
 
+SR04 frontSensorUS(arduinoRuntime, triggerPin, echoPin, maxDistance);
+GP2D120 backSensorIR(arduinoRuntime, BACK_PIN);
 
-const auto oneSecond = 1UL;
+arduino_car::SimpleCarWrapper simpleCarWrapper(car);
+arduino_car::SimpleCarMqttImplementation mqttWrapper(mqtt);
+arduino_car::SerialImplementation serialWrapper;
+arduino_car::FrontDistanceSensorImplementation frontUsSensorWrapper(frontSensorUS);
+arduino_car::BackDistanceSensorImplementation backIrSensorWrapper(backSensorIR);
+
+arduino_car::SimpleCarController simpleCarController(simpleCarWrapper, mqttWrapper, serialWrapper, frontUsSensorWrapper, backIrSensorWrapper);
+
+/*const auto oneSecond = 1UL;
 const auto triggerPin = 6;
 const auto echoPin = 7;
 const auto maxDistance = 400;
@@ -30,153 +44,56 @@ const int TRIGGER_PIN           = 6; // D6
 const int ECHO_PIN              = 7; // D7
 const unsigned int MAX_DISTANCE = 100;
 const auto BACK_PIN = 3;
-const auto stoppingSpeed = 0; 
+const auto stoppingSpeed = 0;
 const auto stopDistanceFront = 80;
 const auto stopDistanceBack = 100;
 const int stopAngle = 0;
-const int autoSpeed = 65;
+const int autoSpeed = 60;
 const int autoAngle = 90;
-const int turnRight = 1;
-const int turnLeft = -1;
 int autoDriving = 0;
 int carSpeed = 0;
-boolean isObstacleDetectedPublished = false; //keeps track of when an obstacle has been detected message is published to mqtt
-
-
-SR04 frontSensorUS(arduinoRuntime, triggerPin, echoPin, maxDistance);
-GP2D120 backSensorIR(arduinoRuntime, BACK_PIN);
+boolean isObstacleDetectedPublished = false; *///keeps track of when an obstacle has been detected message is published to mqtt
 
 std::vector<char> frameBuffer;
 
 void setup() {
-  Serial.begin(9600);
+    serialWrapper.begin(9600);
 #ifdef __SMCE__
-  //mqtt.begin("3.138.188.190", 1883, WiFi);
+    //mqtt.begin("3.138.188.190", 1883, WiFi);
   //mqtt.begin("aerostun.dev", 1883, WiFi);
-  mqtt.begin(WiFi); // Will connect to localhost
+  mqttWrapper.begin(); // Will connect to localhost
   Camera.begin(QQVGA, RGB888, 15);
   frameBuffer.resize(Camera.width() * Camera.height() * Camera.bytesPerPixel());
 #else
-  mqtt.begin(net);
+    mqttWrapper.begin();
 #endif
-  if (mqtt.connect("arduino", "public", "public")) {
-    Serial.println("GENERAL CONNECTION");
-    mqtt.subscribe("/smartcar/switchServer", 0);
-    mqtt.subscribe("/smartcar/control/#", 0);
-    mqtt.onMessage(+[](String& topic, String& message) {
-      Serial.println("Got initial message");
-      if (topic == "/smartcar/switchServer"){
-        Serial.println("SWITCHED");
-        mqtt.setHost("3.138.188.190", 1883);
-        mqtt.connect("arduino", "public", "public");
-        mqtt.subscribe("/smartcar/control/#", 0);
-        mqtt.publish("test", "test");
-      }
-      if (topic == "/smartcar/control/speed" && autoDriving == 0) {
-        carSpeed = message.toInt();
-        if (!(obstacleAvoidance())){
-          car.setSpeed(carSpeed);
-        }
-      } else if (topic == "/smartcar/control/turning" && autoDriving == 0) {
-        car.setAngle(message.toInt());
-      } else if (topic == "/smartcar/control/auto") {
-        autoDriving = message.toInt();
-        Serial.println(autoDriving);
-        if (autoDriving == 0){
-          car.setSpeed(stoppingSpeed);
-          car.setAngle(stopAngle);
-          carSpeed = stoppingSpeed;
-        } else {
-          car.setSpeed(autoSpeed);
-          carSpeed = autoSpeed;
-        }
-      } else {
-        Serial.println(topic + " " + message);
-      }
-    });
-  }
+    simpleCarController.registerManualControl();
 }
 
 void loop() {
-  if (mqtt.connected()) {
-    mqtt.loop();
-    const auto currentTime = millis();
+    if (mqttWrapper.connected()) {
+        mqttWrapper.loop();
+        const auto currentTime = millis();
 #ifdef __SMCE__
-    static auto previousTransmission = 0UL;
+        static auto previousTransmission = 0UL;
     if (currentTime - previousTransmission >= oneSecond) {
       previousTransmission = currentTime;
       Camera.readFrame(frameBuffer.data());
-      mqtt.publish("Camera_Stream", frameBuffer.data(), frameBuffer.size(), false, 0);
+      mqttWrapper.publish("Camera_Stream", frameBuffer.data(), frameBuffer.size(), false, 0);
     }
 #endif
-  }
+    }
 #ifdef __SMCE__
-  // Avoid over-using the CPU if we are running in the emulator
-  if (obstacleAvoidance()) {
+    // Avoid over-using the CPU if we are running in the emulator
+  if (simpleCarController.registerObstacleAvoidance()) {
          if (autoDriving == 1){
-              autonomousMoving();
+              simpleCarController.registerAutonomousMoving();
           }
       }
   delay(35);
 #endif
 }
 
-boolean obstacleAvoidance(){
-  boolean isObstacleDetected = false;
-  
-  auto frontDistanceFromObject = frontSensorUS.getDistance();
-  auto backDistanceFromObject = backSensorIR.getDistance();
-  
-  boolean isFrontDetected = frontDistanceFromObject < stopDistanceFront && frontDistanceFromObject > 1 && (carSpeed > 0);
-  boolean isBackDetected = backDistanceFromObject < stopDistanceBack && backDistanceFromObject > 1 && (carSpeed < 0);
-  
-  
-  if (isFrontDetected || isBackDetected) {
-    sendObstacleDetectedNotification(true);
-    car.setSpeed(stoppingSpeed);
-    isObstacleDetected = true;
-  } else {
-    sendObstacleDetectedNotification(false);
-  }
-  
-  return isObstacleDetected;
-}
 
 
-void sendObstacleDetectedNotification(boolean shouldSend){
-  if (shouldSend){
-    if (!isObstacleDetectedPublished){
-      mqtt.publish("/smartcar/obstacle");
-      isObstacleDetectedPublished = true;
-    }
-  } else {
-      isObstacleDetectedPublished = false;
-  }
-}  
 
-void autonomousMoving(){
-        car.setSpeed(stoppingSpeed);
-        delay(500);
-        car.setSpeed(-autoSpeed);
-        delay(500);
-        car.setSpeed(stoppingSpeed);
-        delay(500);
-        turning(turnLeft);
-if (obstacleAvoidance()) {
-    turning(turnRight);
-    turning(turnRight);
-    if (obstacleAvoidance()) {
-            turning(turnRight);
-          }
-        }
-        car.setSpeed(autoSpeed);
-}
-
-void turning(int direction){
-  car.setSpeed(autoSpeed);
-  car.setAngle(direction*autoAngle);
-  delay(2000);
-  car.setSpeed(stoppingSpeed);
-  car.setAngle(stopAngle);
-  delay(500);
-}
